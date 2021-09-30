936537afff382ab84cf6333f12d2a42f99b5badc
matthieu gayon <matthieu.gayon@gmail.com>
2021-09-30 12:42:30 +0300

    now generate an iced Color

--- src/lib.rs
+++ src/lib.rs
@@ -1,73 +1,10 @@
-//! This crate provides the `hex!` macro for converting hexadecimal string literals
-//! to a byte array at compile time.
-//!
-//! It accepts the following characters in the input string:
-//!
-//! - `'0'...'9'`, `'a'...'f'`, `'A'...'F'` — hex characters which will be used
-//!     in construction of the output byte array
-//! - `' '`, `'\r'`, `'\n'`, `'\t'` — formatting characters which will be
-//!     ignored
-//!
-//! Additionally it accepts line (`//`) and block (`/* .. */`) comments. Characters
-//! inside of those are ignored.
-//!
-//! # Examples
-//! ```
-//! # #[macro_use] extern crate hex_literal;
-//! // the macro can be used in const context
-//! const DATA: [u8; 4] = hex!("01020304");
-//! # fn main() {
-//! assert_eq!(DATA, [1, 2, 3, 4]);
-//!
-//! // it understands both upper and lower hex values
-//! assert_eq!(hex!("a1 b2 c3 d4"), [0xA1, 0xB2, 0xC3, 0xD4]);
-//! assert_eq!(hex!("E5 E6 90 92"), [0xE5, 0xE6, 0x90, 0x92]);
-//! assert_eq!(hex!("0a0B 0C0d"), [10, 11, 12, 13]);
-//! let bytes = hex!("
-//!     00010203 04050607
-//!     08090a0b 0c0d0e0f
-//! ");
-//! assert_eq!(bytes, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
-//!
-//! // it's possible to use several literals (results will be concatenated)
-//! let bytes2 = hex!(
-//!     "00010203 04050607" // first half
-//!     "08090a0b 0c0d0e0f" // second hald
-//! );
-//! assert_eq!(bytes2, bytes);
-//!
-//! // comments can be also included inside literals
-//! assert_eq!(hex!("0a0B // 0c0d line comments"), [10, 11]);
-//! assert_eq!(hex!("0a0B // line comments
-//!                  0c0d"), [10, 11, 12, 13]);
-//! assert_eq!(hex!("0a0B /* block comments */ 0c0d"), [10, 11, 12, 13]);
-//! assert_eq!(hex!("0a0B /* multi-line
-//!                          block comments
-//!                       */ 0c0d"), [10, 11, 12, 13]);
-//! # }
-//! ```
-#![doc(
-    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/hex-literal/0.3.3"
-)]
-
-mod comments;
 extern crate proc_macro;
 
 use core::panic;
 use std::vec::IntoIter;
+use quote::quote;
+use proc_macro::{Delimiter, Literal, TokenStream, TokenTree};
 
-use proc_macro::{Delimiter, Group, Literal, Punct, Spacing, TokenStream, TokenTree};
-
-use crate::comments::{Exclude, ExcludingComments};
-
-/// Strips any outer `Delimiter::None` groups from the input,
-/// returning a `TokenStream` consisting of the innermost
-/// non-empty-group `TokenTree`.
-/// This is used to handle a proc macro being invoked
-/// by a `macro_rules!` expansion.
-/// See https://github.com/rust-lang/rust/issues/72545 for background
 fn ignore_groups(mut input: TokenStream) -> TokenStream {
     let mut tokens = input.clone().into_iter();
     loop {
@@ -82,40 +19,25 @@ fn ignore_groups(mut input: TokenStream) -> TokenStream {
 }
 
 struct TokenTreeIter {
-    buf: ExcludingComments<IntoIter<u8>>,
+    buf: IntoIter<u8>,
     is_punct: bool,
 }
 
 impl TokenTreeIter {
     fn new(input: Literal) -> Self {
-        let mut hex_symbol_free = input.to_string().replace("#", "").to_lowercase();
-        let count = hex_symbol_free.chars().count();
-
-        if count == 5 {
-            let temp_string = hex_symbol_free.replace('"', "");
-            hex_symbol_free = temp_string.chars()
-                .map(|c| c.to_string()
-                .repeat(2))
-                .collect::<Vec<String>>()
-                .join("");
-
-            hex_symbol_free.insert(0, '"');
-            hex_symbol_free.push('"');
-        } else if count != 7 {
-            panic!("string length should equal to 3 or 6");
-        }
-        
-        let mut buf: Vec<u8> = hex_symbol_free.into();
+        let mut buf: Vec<u8> = input.to_string().into();
 
         match buf.as_slice() {
             [b'"', .., b'"'] => (),
             _ => panic!("expected string literals"),
         };
+
+        // remove " char
         buf.pop();
-        let mut iter = buf.into_iter().exclude_comments();
-        iter.next();
+        buf.remove(0);
+
         Self {
-            buf: iter,
+            buf: buf.into_iter(),
             is_punct: false,
         }
     }
@@ -123,6 +45,7 @@ impl TokenTreeIter {
     fn next_hex_val(&mut self) -> Option<u8> {
         loop {
             let v = self.buf.next()?;
+
             let n = match v {
                 b'0'..=b'9' => v - 48,
                 b'A'..=b'F' => v - 55,
@@ -136,11 +59,11 @@ impl TokenTreeIter {
 }
 
 impl Iterator for TokenTreeIter {
-    type Item = TokenTree;
+    type Item = Option<f32>;
 
-    fn next(&mut self) -> Option<TokenTree> {
-        let v = if self.is_punct {
-            TokenTree::Punct(Punct::new(',', Spacing::Alone))
+    fn next(&mut self) -> Option<Option<f32>> {
+        let v =  if self.is_punct {
+            Some(None)
         } else {
             let p1 = self.next_hex_val()?;
             let p2 = match self.next_hex_val() {
@@ -148,24 +71,42 @@ impl Iterator for TokenTreeIter {
                 None => panic!("expected even number of hex characters"),
             };
             let val = (p1 << 4) + p2;
-            TokenTree::Literal(Literal::f32_suffixed(val as f32 / 255.))
+            Some(Some(val as f32 / 255.))
         };
+        
         self.is_punct = !self.is_punct;
-        Some(v)
+
+        v
     }
 }
 
-/// Macro for converting sequence of string literals containing hex-encoded data
-/// into an array of bytes.
+/// Macro for converting sequence an hexadecimal color string 
+/// into a Color { r: f32, g: f32, b: f32 }
 #[proc_macro]
-pub fn hex_color(input: TokenStream) -> TokenStream {
-    let mut out_ts = TokenStream::new();
+pub fn hex(input: TokenStream) -> TokenStream {
+    let mut out_ts: Option<TokenTreeIter> = None;
+
     for tt in ignore_groups(input) {
         let iter = match tt {
             TokenTree::Literal(literal) => TokenTreeIter::new(literal),
             _ => panic!("expected string literals"),
         };
-        out_ts.extend(iter);
+        out_ts = Some(iter);
     }
-    TokenStream::from(TokenTree::Group(Group::new(Delimiter::Bracket, out_ts)))
+
+    let res: Vec<f32> = out_ts.unwrap()
+        .into_iter()
+        .filter(|val| val.is_some())
+        .map(|val| val.unwrap())
+        .collect();
+
+    let r = res[0];
+    let g = res[1];
+    let b = res[2];
+
+    let tokens = quote! {
+        Color { r: #r, g: #g, b: #b, a: 1. }
+    };
+
+    tokens.into()
 }
--- tests/hex_color_macro.rs
+++ tests/hex_color_macro.rs
@@ -1,35 +1,8 @@
-use hex_color::*;
-
-#[derive(Debug)]
-pub struct Color {
-    pub r: f32,
-    pub g: f32,
-    pub b: f32,
-    pub a: f32
-}
-
-impl Color {
-    pub fn from_u8_array(rgb_array: [u8; 3]) -> Color {
-        Color { 
-            r: f32::from(rgb_array[0]) / 255.0, 
-            g: f32::from(rgb_array[1])/ 255.0, 
-            b: f32::from(rgb_array[2]) / 255.0, 
-            a: 1. 
-        }
-    }
-
-    pub const fn from_rgb_array(rgb_array: [f32; 3]) -> Color {
-        Color { r: rgb_array[0], g: rgb_array[1], b: rgb_array[2], a: 1. }
-    }
-}
-
-const fn color(rgb_array: [f32; 3]) -> Color {
-    return Color::from_rgb_array(rgb_array)
-}
+use hex::*;
+use iced_native::Color;
 
 #[test]
 fn test_macro(){
-    const white: Color = color(hex_color!("#B00"));
-
+    const white: Color = hex!("00ffff");
     println!("{:?}", white);
 }
